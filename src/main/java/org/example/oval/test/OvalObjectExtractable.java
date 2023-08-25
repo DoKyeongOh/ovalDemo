3e352f92166604f08f54634549b57b90745bc3e8
dkproh <dkproh@gmail.com>
2023-08-25 19:01:32 +0900

    refactor - 불필요 클래스 삭제

--- src/main/java/org/example/oval/test/OvalObjectExtractable.java
+++
@@ -1,9 +0,0 @@
-package org.example.oval.test;
-
-import org.mitre.oval.xmlschema.oval_definitions_5.ObjectType;
-
-import java.util.Map;
-
-public interface OvalObjectExtractable {
-    ObjectType extractObject(Map<String, ObjectType> objectTypeMap);
-}
--- src/main/java/org/example/oval/test/OvalStateExtractable.java
+++
@@ -1,9 +0,0 @@
-package org.example.oval.test;
-
-import org.mitre.oval.xmlschema.oval_definitions_5.StateType;
-
-import java.util.Map;
-
-public interface OvalStateExtractable {
-    StateType extractState(Map<String, StateType> stateTypeMap);
-}
