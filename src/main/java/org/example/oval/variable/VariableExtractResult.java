57a20160089cbd3e0a6bb1a2cf6e14bb7da36df6
dkproh <dkproh@gmail.com>
2023-09-06 16:41:54 +0900

    feat - VariableExtractResult 추가

---
+++ src/main/java/org/example/oval/variable/VariableExtractResult.java
@@ -0,0 +1,47 @@
+package org.example.oval.variable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VariableExtractResult {
+
+    private List<Object> extractedItems;
+
+    private VariableExtractResultType resultType;
+
+    public VariableExtractResult() {
+        extractedItems = new ArrayList<>();
+        resultType = VariableExtractResultType.COMPLETE;
+    }
+
+    public VariableExtractResult(List<Object> extractedItems) {
+        this.extractedItems = extractedItems;
+        resultType = VariableExtractResultType.COMPLETE;
+    }
+
+    public VariableExtractResult(VariableExtractResultType resultType) {
+        this.resultType = resultType;
+        extractedItems = new ArrayList<>();
+    }
+
+    public List<Object> getExtractedItems() {
+        return extractedItems;
+    }
+
+    public void setExtractedItems(List<Object> extractedItems) {
+        this.extractedItems = extractedItems;
+    }
+
+    public VariableExtractResultType getResultType() {
+        return resultType;
+    }
+
+    public void setResultType(VariableExtractResultType resultType) {
+        this.resultType = resultType;
+    }
+
+    public enum VariableExtractResultType {
+        ERROR, COMPLETE, INCOMPLETE, DOES_NOT_EXIST, NOT_COLLECTED, NOT_APPLICABLE
+    }
+
+}
