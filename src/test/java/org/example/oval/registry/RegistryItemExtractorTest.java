f087e94df439d6515486ded05d720f6a8f7cf2f0
dkproh <dkproh@gmail.com>
2023-09-06 18:00:25 +0900

    feat - 레지스트리 값 읽는중

--- src/test/java/org/example/oval/registry/RegistryItemExtractorTest.java
+++ src/test/java/org/example/oval/registry/RegistryItemExtractorTest.java
@@ -1,27 +1,46 @@
 package org.example.oval.registry;
 
+import com.sun.jna.platform.win32.Advapi32Util;
+import com.sun.jna.platform.win32.WinReg;
 import org.junit.Test;
-import org.mitre.oval.xmlschema.oval_system_characteristics_5_windows.RegistryItem;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.prefs.AbstractPreferences;
+import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 public class RegistryItemExtractorTest {
 
     @Test
-    public void test() throws IOException {
-        Runtime runtime = Runtime.getRuntime();
-        Process process = runtime.exec("reg query \"HKEY_CURRENT_USER\\Environment\" /v ANT_HOME");
+    public void test() throws IOException, BackingStoreException {
+        String hive = "HKEY_LOCAL_MACHINE";
+        String key = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion";
+        String name = "BuildLab";
+        readRegistryFromPreferences(hive, key, name);
+    }
 
-        RegistryItem registryItem = new RegistryItem();
+    private void readRegistryValue(String hive, String key, String name) throws IOException {
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec("reg query \""+hive+"\\"+key+"\" /ve ");
 
         String line = null;
         BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
         while ((line = br.readLine()) != null) {
             System.out.println(line);
         }
+    }
+    private void readRegistryFromPreferences(String hive, String key, String name) throws BackingStoreException {
+        String registryPath = hive + "\\" + key;
+        Preferences preferences = Preferences.systemRoot();
+        String[] subKeys = preferences.childrenNames();
+        for (String subKey : subKeys) {
+            System.out.println(subKey);
+        }
+
+        String s = Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, key, name);
+        System.out.println(s);
     }
 
 }
